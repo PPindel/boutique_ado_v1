d86612958dd097ad39c7526a1bb553e4c221fcd6
PPindel <przemyslaw.pindel@gmail.com>
2023-06-24 13:56:41 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NMWGmCUKjlbqdbxsuzTnLcXkHrfcgX2sN9MyINFd8G7buB4ZZBo00ELFUhCpFZymlQ1bGyqZx1Yq55e4U41CXGI001nwa50Sq',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
