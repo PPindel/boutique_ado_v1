d76aae03235af7d5f87ca264eb8b9cfa0b40b6c0
PPindel <przemyslaw.pindel@gmail.com>
2023-07-05 10:42:31 +0000

    Settings update for deployment to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-ppindel-boutiqueadov1-ehfl0tz3y2q.ws-eu101.gitpod.io', 'boutique-ado-ppindel-ff7909b1c3db.herokuapp.com']
 
